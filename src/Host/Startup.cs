457fb160b4b7d0ae352060a6287bbc5bb29c0370
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-05-16 21:56:11 +0200

    Add appsettings.

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -8,7 +8,13 @@ public class Startup : IStartup
 {
     public void ConfigureConfiguration(IConfigurationBuilder configurationBuilder, IHostEnvironment host)
     {
+        const string configurationFolder = "Configurations";
 
+        configurationBuilder
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile($"{configurationFolder}/appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"{configurationFolder}/appsettings.{host.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
     }
 
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
