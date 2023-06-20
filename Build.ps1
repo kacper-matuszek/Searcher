1c3ab3d041c25226b08154562255a5ea4dcfe0a7
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-06-20 22:35:39 +0200

    Add cake scripts.

---
+++ Build.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+
+Set-Location -LiteralPath $PSScriptRoot
+
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
+$env:DOTNET_NOLOGO = '1'
+
+dotnet tool install Cake.Tool --version 2.2.0
+
+dotnet tool restore
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+dotnet cake ./cake/build.cake @args --project=Searcher
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
