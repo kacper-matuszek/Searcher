44635719b5929235b820c51483454c03282f2153
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-08-11 19:46:46 +0200

    Add tag tests.

---
+++ test/Model.Tests/TagTests.cs
@@ -0,0 +1,26 @@
+﻿using Searcher.Model.Shared.ValueObjects;
+using System;
+using Xunit;
+
+namespace Searcher.Model.Tests;
+
+public partial class TagTests
+{
+    [Fact]
+    public void CorrectValue_Should_CreateInstanceOfTag()
+    {
+        var value = "value";
+
+        var tag = new Tag(value);
+
+        Assert.Equal(value, tag.Value);
+    }
+
+    [Fact]
+    public void EmptyValue_Should_ThrowArgumentException()
+    {
+        var value = string.Empty;
+
+        Assert.Throws<ArgumentException>(() => new Tag(value));
+    }
+}
